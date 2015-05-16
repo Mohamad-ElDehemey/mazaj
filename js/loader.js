741b1af87517087f41d3225fafb06c900f7397c4
fox564 <mostafa_run@yahoo.com>
2015-05-16 08:49:39 +0000

    -added play list drop-down menu playlists.html

--- js/loader.js
+++ js/loader.js
@@ -1,5 +1,5 @@
+var chromeerror= '<br> if you are using google chrome, try running it with --allow-file-access-from-files <br> or You can follow their progress on the issue <a href="code.google.com/p/chromium/issues/detail?id=40787">here:</a>';
 $(document).ready(function(){
-    var chromeerror= '<br> if you are using google chrome, try running it with --allow-file-access-from-files <br> or You can follow their progress on the issue <a href="code.google.com/p/chromium/issues/detail?id=40787">here:</a>';
     $('#home').load('Home.html', '', function(response, status, xhr) {
         if (status == 'error') {
             var msg = "Sorry but there was an error: ";
@@ -83,4 +83,15 @@ $(document).on("click", "#homepage", "", function(){
             $("#Likes").html(msg + xhr.status + " " + xhr.statusText + chromeerror);
         }
     });    
+});
+$(document).on("click", "#playlistload", "", function(){
+    $('.dropdown-playlist').load('playlists.html', '', function(response, status, xhr) {
+        if (status == 'error') {
+            var msg = "Sorry but there was an error: ";
+            $(".dropdown-playlist").html(msg + xhr.status + " " + xhr.statusText + chromeerror);
+        }
+    });    
+
+    $('.dropdown-playlist').css('max-height', ($(window).height() - $('.dropdown-playlist').offset().top) + 'px');
+    $('.dropdown-playlist').css('height', ($(window).height() - $('.dropdown-playlist').offset().top) + 'px');
 });
